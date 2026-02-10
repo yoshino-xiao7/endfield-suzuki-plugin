98499e533e23610af05f122289d1fd80a1e64a32
yukiryou <xiao7@yukiryou.top>
2026-02-11 05:51:44 +0800

    修复图片渲染

--- model/render.js
+++ model/render.js
@@ -109,49 +109,17 @@ export default class Render {
         // Generate HTML
         const html = generateHtml(renderData)
 
+        // Read CSS file to inject directly
+        const cssPath = path.join(PLUGIN_ROOT, 'resources', 'style.css')
+        const css = fs.readFileSync(cssPath, 'utf8')
+
         // Generate Image using Yunzai puppeteer
-        // Note: 'endfield-suzuki-plugin' is the name of the plugin folder for resource lookup
         const img = await puppeteer.screenshot('endfield-suzuki-plugin', {
-            tplFile: path.join(PLUGIN_ROOT, 'resources', 'card.html'), // We are injecting HTML content directly, but standard usage might require a file. 
-            // However, Yunzai puppeteer usually takes 'html' param or 'tplFile'.
-            // Let's rely on creating a temporary HTML file or using the `html` content directly if supported, 
-            // OR standardized approach: save specific data to pass to a template file. 
-            // Yunzai's puppeteer works best with passing data to an art-template file.
-            // Since we wanted to avoid template engines complexity for now, let's write the HTML string to a file and render that? 
-            // BETTER: Yunzai puppeteer supports `html` parameter for direct HTML string content?
-            // Checking standard Yunzai puppeteer usage: usually `puppeteer.screenshot("name", data)`. 
-            // It looks for /resources/name/html.html.
-
-            // Re-evaluating strategy: 
-            // To be robust and use standard Yunzai method, let's just make the `generateHtml` function produce the FULL HTML string,
-            // and maybe we can pass it. 
-            // BUT, writing a custom render method using `puppeteer.screenshot` often requires `tplFile` or standard folder structure.
-
-            // Let's try passing the HTML content directly via a custom saving mechanism if `puppeteer` allows, 
-            // OR easier: just use the generated HTML string and let puppeteer render it via `page.setContent`.
-            // Yunzai's `puppeteer.screenshot` wrapper is opinionated.
-
-            // Alternative: Write the HTML to a temp file in resources and point to it.
-            // OR: Use the `saveHtml` param if available.
-
-            // Let's stick to the simplest: We have `generateHtml` return the exact HTML. 
-            // We can use a trick: standard `puppeteer` class in Yunzai might not expose `setContent` easily via `screenshot`.
-            // Let's look at `puppeteer.js` source if we could... but we can't assume access to it.
-            // Standard approach:
-            // 1. Create `resources/card.html` which is an `art-template` or `ejs` template.
-            // 2. Pass `renderData` to `puppeteer.screenshot`.
-
-            // Since I promised to generate HTML in JS to avoid template dependency issues (if `art-template` isn't installed? it usually is in Yunzai),
-            // I will actually implement `generateHtml` to return the full HTML string, 
-            // and I will save it to `data/temp.html` and render that URL maybe? No, file protocol issues.
-
-            // Let's go with the `art-template` approach as it is standard in Yunzai.
-            // I will create `resources/card.html` as an art-template entry.
-            // And use `puppeteer.screenshot` passing order data.
-
+            tplFile: path.join(PLUGIN_ROOT, 'resources', 'card.html'),
             saveId: 'card',
             imgType: 'jpeg',
             quality: 90,
+            style: css,
             ...renderData
         })
 
